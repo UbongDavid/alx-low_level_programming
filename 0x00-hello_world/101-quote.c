a3f74ee1e95f34b616a2b36416e1a9bb88f0b612
ubongDavid <shiinird@gmail.com>
2022-06-30 21:58:24 -0700

    TC9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 /**
- * main - prints a standard error- "and that piece of art is useful"
+ * main - prints a standard error
  *
  * Return: Always 1 (Success)
  */
@@ -11,15 +11,14 @@ int main(void)
 
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
-
-}
 
 
 fprintf(stderr, "and that piece of art is useful\" - \
 Dora Korpar, 2015-10-19\n");
+
 
 return (1);
 }
