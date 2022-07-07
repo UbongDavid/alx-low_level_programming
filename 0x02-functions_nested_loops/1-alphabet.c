c943a851c42f838636ca0175f9499dbf6c8f5400
ubongDavid <shiinird@gmail.com>
2022-07-06 20:48:14 -0700

    commit 2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_alphabet -> This function prints all letters of the alphbet
+ * @c: parameter of type char
+ * Return: Void.
  */
 
 void print_alphabet(void)
