cd0b95a74f7c28c54210c1fcbff6c70f1ac8355c
ubongDavid <shiinird@gmail.com>
2022-07-07 21:49:27 -0700

    commit9

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+
+/**
+ * print_number - This function prints an integer
+ * @n: A Variable parameter of type integer
+ * Return- Nil
+ */
+
+void print_number(int n)
+
+{
+	unsigned int n1 = 0;
+
+	if  (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
+}
