20f243c4e229b103e87e457ebcae7bb7ce0ac4ad
ubongDavid <shiinird@gmail.com>
2022-07-14 19:21:04 -0700

    Commit11

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,35 +1,29 @@
-#include "main.h"
 #include <stdio.h>
-/**
- * print_to_98 - from n to 98
- * @n: integer
- *
- * Return: this function print from n to 98
- */
-void print_to_98(int n)
-{
-	if (n < 98)
-	{
-		for (; n <= 98; n++)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
 
-	printf("\n");
+/**
+ * main - finds and prints the sum of the even-valued terms
+ *  * followed by a new line
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+	printf("%lu\n", sum);
+	return (0);
 }
