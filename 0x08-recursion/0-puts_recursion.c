36a3d8fbd2247658a543e582e90a00640a346810
uddes <shiinird@gmail.com>
2022-07-19 16:57:27 -0700

    Commit0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * *_memset - prints a string followed by a newline
+ * _puts_recursion - prints a string followed by a newline
  * @s: a string variable
  * Return: Void
  */
 
 void _puts_recursion(char *s)
 {
-	if (! *s)
+	if (!*s)
 	{
 		_putchar('\n');
 		return;
 	}
- 	_putchar(*s);
+	_putchar(*s);
 	s++;
 	_puts_recursion(s);
 }
