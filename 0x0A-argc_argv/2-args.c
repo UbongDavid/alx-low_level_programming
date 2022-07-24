876bfd868736463649f385eee13b2d33701b0e91
uddes <shiinird@gmail.com>
2022-07-24 14:00:35 -0700

    Commit2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints its name
+ * @argc: total number of passed command-line arguments
+ * @argv: array containing passed CLI arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	int argumentIndex;
+
+	for ((argumentIndex = 0); (argumentIndex <= argc - 1); argumentIndex++)
+	{printf("%s\n", argv[argumentIndex]);}
+	return (0);
+}
