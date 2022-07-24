a13e5eddbebc1492fbfb6c0ebf260487dc940346
uddes <shiinird@gmail.com>
2022-07-24 13:23:43 -0700

    Commit0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints its name
+ * @argc: total number of passed command-line arguments
+ * @argv: array containing passed CLI arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[(argc * 0)]);
+	return (0);
+}
