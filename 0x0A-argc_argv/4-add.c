ef0087ea4654d89deae1bbac1fe15e4a7a04348c
uddes <shiinird@gmail.com>
2022-07-26 00:54:02 -0700

    Commit4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,22 +1,33 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - A program that prints the product of two numbers
+ * main - A program that prints the sum of two numbers
  * @argc: total number of passed command-line arguments
  * @argv: array containing passed CLI arguments
- * Return: Always 0
+ * Return: Varies
  */
 
 int main(int argc, char **argv)
 {
-	if (argc != 3)
+	int argumentSum = 0;
+
+	if ((argc == 1))
 	{
-		printf("Error\n");
-		return (1);
-	}
-	else
-	{
-		printf("i%", (*argv) * (*(argv+1)));
+		printf("0\n");
 		return (0);
 	}
+
+	while (argc-- && argc != 0)
+	{
+		if (!atoi(argv[argc]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		argumentSum += atoi(argv[argc]);
+	}
+	printf("%i\n", argumentSum);
+	return (0);
 }
